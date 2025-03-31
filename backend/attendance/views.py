d2163584128e8501aa4e5f3f4a4850fe081c1de3
Pradhansumit <pradhansumit026@gmail.com>
2025-03-31 06:50:55 +0000

    One time checkin checkout for a particular day. Strict mode.

--- backend/attendance/views.py
+++ backend/attendance/views.py
@@ -1,4 +1,6 @@
-from accounts.permission import Employee_Permission
+from tabnanny import check
+
+from accounts.permission import Admin_Permission, Employee_Permission
 from attendance.models import Attendance
 from attendance.serializer import AttendanceSerializer
 from django.contrib.auth import get_user_model
@@ -18,25 +20,41 @@ class Employee_ChecksIn_Attendance(APIView):
             user = USER.objects.get(username=request.user)
 
             # In case if an employee forgets to checkout and trying to check in.
-            attendance = Attendance.objects.filter(
-                user=user.id, check_out__isnull=True
-            ).first()
+            attendance = Attendance.objects.filter(user=user.id, check_out__isnull=True)
+
             if attendance:
-                attendance.check_out = timezone.now()
-                attendance.save()
-
-            _data = request.data.copy()
-
-            _data["user"] = user.id
-
-            serializer = AttendanceSerializer(data=_data)
-            if serializer.is_valid():
-                serializer.save()
-                return Response(status=status.HTTP_201_CREATED)
-            else:
                 return Response(
-                    serializer.errors, status=status.HTTP_500_INTERNAL_SERVER_ERROR
+                    {"error": "You have not checkout your previous checkin."},
+                    status=status.HTTP_400_BAD_REQUEST,
                 )
+
+            # In case if an employee already checkout for the current date (at any time),
+            # then it is invalid for employee to mark any other attendance.
+            is_checkedout = Attendance.objects.filter(
+                user=user.id,
+                check_in__date=timezone.now().date(),
+                check_out__date=timezone.now().date(),
+            )
+            print(is_checkedout)
+            if not is_checkedout:
+                _data = request.data.copy()
+
+                _data["user"] = user.id
+
+                serializer = AttendanceSerializer(data=_data)
+                if serializer.is_valid():
+                    serializer.save()
+                    return Response(status=status.HTTP_201_CREATED)
+                else:
+                    return Response(
+                        serializer.errors, status=status.HTTP_500_INTERNAL_SERVER_ERROR
+                    )
+
+            return Response(
+                {"error": "Attendance is already marked."},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+
         except Exception as e:
             return Response(str(e), status=status.HTTP_400_BAD_REQUEST)
 
@@ -46,13 +64,30 @@ class Employee_ChecksOut_Attendance(APIView):
 
     def post(self, request) -> Response:
         try:
-            user = request.user.id
+            user = USER.objects.get(username=request.user)
             attendance = Attendance.objects.filter(
-                user=user.id, check_out__is_null=True
+                user=user.id, check_out__isnull=True
             ).first()
-            attendance.check_out = timezone.now()
-            attendance.save()
-            return Response(status=status.HTTP_200_OK)
+            if attendance:
+                attendance.check_out = timezone.now()
+                attendance.save()
+                return Response(status=status.HTTP_200_OK)
 
+            return Response(
+                {"error": "No check-in to mark check-out."},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+
+        except Exception as e:
+            return Response(str(e), status=status.HTTP_400_BAD_REQUEST)
+
+
+class Admin_View_TodaysAttendance(APIView):
+    permission_classes = [Admin_Permission]
+
+    def get(self, request) -> Response:
+        try:
+
+            return Response()
         except Exception as e:
             return Response(str(e), status=status.HTTP_400_BAD_REQUEST)

ac6b53be6a89bf9c7fc0d97197b77a3a726ee698
Pradhansumit <pradhansumit026@gmail.com>
2025-07-16 12:17:17 +0000

    f: Add salary modal and api call.

--- frontend/src/components/Admin/Payment/AddSalary.jsx
+++ frontend/src/components/Admin/Payment/AddSalary.jsx
@@ -1,148 +1,160 @@
-import React, { useEffect, useState } from "react";
+import React, {useEffect, useState} from "react";
 import api from "../../../config/api";
-import { FaSearch } from "react-icons/fa";
-import { FaPlus } from "react-icons/fa6";
-import { MdFileDownload } from "react-icons/md";
+import {FaRupeeSign, FaSearch} from "react-icons/fa";
+import {FaPlus} from "react-icons/fa6";
+import {MdFileDownload} from "react-icons/md";
 import exportToCSV from "../../../utils/exportToCSV";
-import { FaRupeeSign } from "react-icons/fa";
 import AddSalaryModal from "./AddSalaryModal";
 import URLS from "../../../utils/urls";
+import Breadcrumb from "../../UI/Breadcrumb.jsx";
 
 export default function AddSalary() {
-  const [isLoading, setIsLoading] = useState(true);
-  const [apiData, setApiData] = useState([]);
-  const [search, setSearch] = useState("");
-  const [employees, setEmployees] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
+    const [apiData, setApiData] = useState([]);
+    const [search, setSearch] = useState("");
+    const [employees, setEmployees] = useState([]);
 
-  const fetchData = async () => {
-    try {
-      const salaryRes = await api.get(URLS.baseSalaryList);
-      const userRes = await api.post(URLS.listUsers, { user: 0 });
-      if (salaryRes.status === 200 && userRes.status === 200) {
-        const salaryData = salaryRes.data;
-        setApiData(salaryData);
+    const fetchData = async () => {
+        try {
+            const salaryRes = await api.get(URLS.baseSalaryList);
+            const userRes = await api.post(URLS.listUsers, {user: 0});
 
-        const paidEmails = new Set(salaryData.map((item) => item.email));
-        const unpaidUsers = userRes.data
-          .filter((user) => !paidEmails.has(user.email))
-          .map((user) => ({
-            name: `${user.first_name} ${user.last_name}`,
-            uuid: user.id,
-          }));
-        console.log("Unpaid Users:", unpaidUsers);
-        setEmployees(unpaidUsers);
-      }
-    } catch (error) {
-      console.log(error);
-    } finally {
-      setIsLoading(false);
-    }
-  };
+            if (salaryRes.status === 200 && userRes.status === 200) {
+                const salaryData = salaryRes.data;
+                console.log("Salary Data:", salaryData);
+                setApiData(salaryData);
 
-  useEffect(() => {
-    fetchData();
-  }, []);
-  return isLoading ? (
-    <span className="loading loading-spinner loading-xl"></span>
-  ) : (
-    <>
-      <div className="overflow-x-auto">
-        <div className="breadcrumbs text-sm">
-          <ul>
-            <li>
-              <a href="/">Home</a>
-            </li>
-            <li>
-              <a href="/employee">Add Salary</a>
-            </li>
-          </ul>
-        </div>
+                const paidEmails = new Set(
+                    salaryData.map((item) => item.email),
+                );
+                const unpaidUsers = userRes.data
+                    .filter((user) => !paidEmails.has(user.email))
+                    .map((user) => ({
+                        name: `${user.first_name} ${user.last_name}`,
+                        uuid: user.id,
+                    }));
+                console.log("Unpaid Users:", unpaidUsers);
+                setEmployees(unpaidUsers);
+            }
+        } catch (error) {
+            console.log(error);
+        } finally {
+            setIsLoading(false);
+        }
+    };
 
-        <div className="flex items-center justify-between bg-blue-50 p-3 mb-3 rounded-xl shadow">
-          <div className="flex items-center gap-3">
-            <p className="text-lg font-semibold">Employees</p>
-            <label className="input">
-              <FaSearch className="text-lg" color="gray" />
-              <input
-                type="search"
-                className="grow"
-                placeholder="Search"
-                onChange={(e) => setSearch(e.target.value)}
-              />
-            </label>
-          </div>
-          <div className="flex items-center gap-2">
-            <button
-              className="btn btn-sm btn-circle btn-success text-white"
-              title="Add"
-              onClick={() => document.getElementById("my_modal_5").showModal()}
-            >
-              <FaPlus className="text-2xl" />
-            </button>
-            <button
-              className="btn btn-sm btn-circle btn-secondary text-blue-500"
-              title="Download"
-              onClick={() => exportToCSV(apiData)}
-            >
-              <MdFileDownload className="text-2xl" color="white" />
-            </button>
-          </div>
-        </div>
-        <table className="table bg-white">
-          {/* head */}
-          <thead>
-            <tr>
-              <th>
-                <label>
-                  <input type="checkbox" className="checkbox" />
-                </label>
-              </th>
-              <th>name</th>
-              <th>email</th>
-              <th>department</th>
-              <th>amount</th>
-            </tr>
-          </thead>
-          <tbody>
-            {apiData
-              .filter(
-                (data) =>
-                  data.name?.toLowerCase().includes(search) ||
-                  data.email?.toLowerCase().includes(search),
-              )
-              .map((data) => (
-                <tr key={data.id}>
-                  <th>
-                    <label>
-                      <input type="checkbox" className="checkbox" />
-                    </label>
-                  </th>
-                  <th className="flex items-center gap-2">
-                    {
-                      <div className="avatar">
-                        <div className="w-12 rounded-full">
-                          <img
-                            src={`${import.meta.env.VITE_API_URL}${
-                              data.profile_pic
-                            }`}
-                          />
-                        </div>
-                      </div>
-                    }
-                    {data.name}
-                  </th>
-                  <td>{data.email}</td>
-                  <td>{data.department}</td>
-                  <td className="">
-                    <FaRupeeSign color="green" className="text-lg inline" />
-                    {data.amount}
-                  </td>
-                </tr>
-              ))}
-          </tbody>
-        </table>
-      </div>
-      <AddSalaryModal />
-    </>
-  );
+    useEffect(() => {
+        fetchData();
+    }, []);
+    return isLoading ? (
+        <span className="loading loading-spinner loading-xl"></span>
+    ) : (
+        <>
+            <div className="overflow-x-auto">
+                <Breadcrumb title={"Add Salary"} link={"add-salary"}/>
+
+                <div className="flex items-center justify-between bg-blue-50 p-3 mb-3 rounded-xl shadow">
+                    <div className="flex items-center gap-3">
+                        <p className="text-lg font-semibold">Employees</p>
+                        <label className="input">
+                            <FaSearch className="text-lg" color="gray"/>
+                            <input
+                                type="search"
+                                className="grow"
+                                placeholder="Search"
+                                onChange={(e) => setSearch(e.target.value)}
+                            />
+                        </label>
+                    </div>
+                    <div className="flex items-center gap-2">
+                        <button
+                            className="btn btn-sm btn-circle btn-success text-white"
+                            title="Add"
+                            onClick={() =>
+                                document
+                                    .getElementById("my_modal_5")
+                                    .showModal()
+                            }
+                        >
+                            <FaPlus className="text-2xl"/>
+                        </button>
+                        <button
+                            className="btn btn-sm btn-circle btn-secondary text-blue-500"
+                            title="Download"
+                            onClick={() => exportToCSV(apiData)}
+                        >
+                            <MdFileDownload
+                                className="text-2xl"
+                                color="white"
+                            />
+                        </button>
+                    </div>
+                </div>
+                <table className="table bg-white">
+                    {/* head */}
+                    <thead>
+                    <tr>
+                        <th>
+                            <label>
+                                <input
+                                    type="checkbox"
+                                    className="checkbox"
+                                />
+                            </label>
+                        </th>
+                        <th>name</th>
+                        <th>email</th>
+                        <th>department</th>
+                        <th>amount</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    {apiData
+                        .filter(
+                            (data) =>
+                                data.name?.toLowerCase().includes(search) ||
+                                data.email?.toLowerCase().includes(search),
+                        )
+                        .map((data) => (
+                            <tr key={data.id}>
+                                <th>
+                                    <label>
+                                        <input
+                                            type="checkbox"
+                                            className="checkbox"
+                                        />
+                                    </label>
+                                </th>
+                                <th className="flex items-center gap-2">
+                                    {
+                                        <div className="avatar">
+                                            <div className="w-12 rounded-full">
+                                                <img
+                                                    src={`${
+                                                        import.meta.env
+                                                            .VITE_API_URL
+                                                    }${data.profile_pic}`}
+                                                />
+                                            </div>
+                                        </div>
+                                    }
+                                    {data.name}
+                                </th>
+                                <td>{data.email}</td>
+                                <td>{data.department}</td>
+                                <td className="">
+                                    <FaRupeeSign
+                                        color="green"
+                                        className="text-lg inline"
+                                    />
+                                    {data.amount}
+                                </td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            </div>
+            <AddSalaryModal getEmployee={employees} refreshData={fetchData()}/>
+        </>
+    );
 }
--- frontend/src/components/Admin/Payment/AddSalaryModal.jsx
+++ frontend/src/components/Admin/Payment/AddSalaryModal.jsx
@@ -1,185 +1,80 @@
-import { useRef } from "react";
+import {useState} from "react";
 import api from "../../../config/api";
 import URLS from "../../../utils/urls";
 // import { toast } from "react-hot-toast";
 
-export default function AddSalaryModal({ getEmployee, setToast }) {
-  const formRef = useRef(null);
+export default function AddSalaryModal({getEmployee, setToast, refreshData}) {
+    const [user_id, setUser_id] = useState(getEmployee[0].uuid);
+    const [salaryAmt, setSalaryAmt] = useState(0);
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    const formData = new FormData(formRef.current);
+    const handleSubmit = async (e) => {
+        e.preventDefault();
 
-    try {
-      const res = await api.post(URLS.addEmployee, formData);
-      if (res.status === 201 || res.status === "201") {
-        document.getElementById("my_modal_5").close();
-        getEmployee();
-        setToast("User added successfully.");
-      }
-    } catch (error) {
-      console.error(error);
-    }
-  };
+        try {
+            const res = await api.post(URLS.baseSalaryList, {user: user_id, amount: parseFloat(salaryAmt)});
+            if (res.status === 201) {
+                document.getElementById("my_modal_5").close();
+                refreshData();
+                setToast("Salary Added Successfully.");
+            }
+        } catch (error) {
+            console.error(error);
+        }
+    };
 
-  return (
-    <dialog id="my_modal_5" className="modal modal-bottom sm:modal-middle">
-      <div className="modal-box w-full max-w-2xl">
-        <h3 className="font-bold text-xl mb-4">Add New Employee</h3>
+    return (
+        <dialog id="my_modal_5" className="modal modal-bottom sm:modal-middle">
+            <div className="modal-box w-full max-w-2xl">
+                <h3 className="font-bold text-xl mb-4">Add Salary</h3>
 
-        <form
-          ref={formRef}
-          onSubmit={handleSubmit}
-          encType="multipart/form-data"
-          className="grid grid-cols-1 md:grid-cols-2 gap-4"
-        >
-          {/* First Name */}
-          <div>
-            <label className="label">First Name</label>
-            <input
-              type="text"
-              className="input input-bordered w-full"
-              name="first_name"
-              required
-            />
-          </div>
+                <form
+                    onSubmit={handleSubmit}
+                    encType="multipart/form-data"
+                    className="grid grid-cols-1 md:grid-cols-2 gap-4"
+                >
+                    <select className="p-2 outline-zinc-700 rounded-lg"
+                            onChange={(e) => {
+                                console.log("Selected Value", e.target.value)
+                                setUser_id(e.target.value)
+                            }}
+                            value={user_id}
+                    >
+                        {getEmployee.map((employee) =>
+                            (
+                                <option
+                                    key={employee.uuid}
+                                    value={employee.uuid}
+                                    className="p-2 outline-zinc-700 rounded-lg"
+                                >
+                                    {employee.name}
+                                </option>)
+                        )}
+                    </select>
+                    <input
+                        type="text"
+                        name="amount"
+                        id="amount"
+                        className="p-2 rounded-lg"
+                        onChange={e => setSalaryAmt(parseInt(e.target.value))}
+                    />
 
-          {/* Last Name */}
-          <div>
-            <label className="label">Last Name</label>
-            <input
-              type="text"
-              className="input input-bordered w-full"
-              name="last_name"
-              required
-            />
-          </div>
-
-          {/* Gender */}
-          <div>
-            <label className="label">Gender</label>
-            <select
-              className="select select-bordered w-full"
-              name="gender"
-              defaultValue="0"
-            >
-              <option value="0">Male</option>
-              <option value="1">Female</option>
-            </select>
-          </div>
-
-          {/* Role */}
-          <div>
-            <label className="label">Role</label>
-            <select
-              className="select select-bordered w-full"
-              name="role"
-              defaultValue="0"
-              required
-            >
-              <option value="0">Employee</option>
-              <option value="1">Admin</option>
-            </select>
-          </div>
-
-          {/* Mobile */}
-          <div>
-            <label className="label">Mobile</label>
-            <input
-              type="tel"
-              className="input input-bordered w-full"
-              name="mobile"
-              pattern="^(\+91[\-\s]?)?[6-9]\d{9}$"
-              title="Enter a valid Indian mobile number. Example: +91 9876543210 or 9876543210"
-              required
-            />
-          </div>
-
-          {/* Email */}
-          <div>
-            <label className="label">Email</label>
-            <input
-              type="email"
-              className="input input-bordered w-full"
-              name="email"
-              required
-            />
-          </div>
-
-          {/* Date of Birth */}
-          <div>
-            <label className="label">Date of Birth</label>
-            <input
-              type="date"
-              className="input input-bordered w-full"
-              name="date_of_birth"
-            />
-          </div>
-
-          {/* Designation */}
-          <div>
-            <label className="label">Designation</label>
-            <input
-              type="text"
-              className="input input-bordered w-full"
-              name="designation"
-            />
-          </div>
-
-          {/* Department */}
-          <div>
-            <label className="label">Department</label>
-            <input
-              type="text"
-              className="input input-bordered w-full"
-              name="department"
-            />
-          </div>
-
-          {/* Education */}
-          <div className="md:col-span-2">
-            <label className="label">Education</label>
-            <textarea
-              className="textarea textarea-bordered w-full"
-              name="education"
-            ></textarea>
-          </div>
-
-          {/* Address */}
-          <div className="md:col-span-2">
-            <label className="label">Address</label>
-            <textarea
-              className="textarea textarea-bordered w-full"
-              name="address"
-            ></textarea>
-          </div>
-
-          {/* Profile Picture
-          <div className="md:col-span-2">
-            <label className="label">Profile Picture</label>
-            <input
-              type="file"
-              className="file-input file-input-bordered w-full"
-              name="profile_pic"
-              accept="image/*"
-            />
-          </div> */}
-
-          {/* Action Buttons */}
-          <div className="modal-action md:col-span-2 flex justify-end mt-4 gap-2">
-            <button type="submit" className="btn btn-primary">
-              Save
-            </button>
-            <button
-              type="button"
-              className="btn"
-              onClick={() => document.getElementById("my_modal_5").close()}
-            >
-              Cancel
-            </button>
-          </div>
-        </form>
-      </div>
-    </dialog>
-  );
+                    {/* Action Buttons */}
+                    <div className="modal-action md:col-span-2 flex justify-end mt-4 gap-2">
+                        <button type="submit" className="btn btn-primary">
+                            Save
+                        </button>
+                        <button
+                            type="button"
+                            className="btn"
+                            onClick={() =>
+                                document.getElementById("my_modal_5").close()
+                            }
+                        >
+                            Cancel
+                        </button>
+                    </div>
+                </form>
+            </div>
+        </dialog>
+    );
 }
